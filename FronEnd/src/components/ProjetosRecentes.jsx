409426f3225288176d34c37d4a3edd986f4df435
Brun0 <brunodev41@gmail.com>
2025-10-01 08:37:39 -0300

    Fazendo a responsividade da dashboard geral e implementando a troca de tema

--- FronEnd/src/components/ProjetosRecentes.jsx
+++ FronEnd/src/components/ProjetosRecentes.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
+import React from "react";
 
 export const ProjetosRecentes = (props) => {
   return (
-    <div className='d-flex align-items-center justify-content-between p-3 rounded-4 border mt-3'>
-                  <div>
-                    <h6 className='mb-1 fw-medium'>{props.nomeProjeto}</h6>
-                    <small className='text-muted'>{props.tempoProjeto}</small>
-                  </div>
-                  <span className='badge bg-success bg-opacity-25 text-success px-3 py-2'>{props.statusProjeto}</span>
-                 </div>
-     
-  )
-}
+    <div className="d-flex align-items-center bg-white justify-content-between p-3 rounded-4 border mt-3">
+      <div>
+        <h6 className="mb-1 fw-medium text-black">{props.nomeProjeto}</h6>
+        <small className="text-secondary">{props.tempoProjeto}</small>
+      </div>
+      <span className="badge bg-success bg-opacity-25 text-success px-3 py-2">
+        {props.statusProjeto}
+      </span>
+    </div>
+  );
+};
