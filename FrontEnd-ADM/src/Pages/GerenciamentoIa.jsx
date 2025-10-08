c70331103f8ccb93963bd85f0fb76eb2a0c6652a
Joaquim <jocaq124@gmail.com>
2025-10-08 08:41:42 -0300

    Ajuste de responsividade no container

--- FrontEnd-ADM/src/Pages/GerenciamentoIa.jsx
+++ FrontEnd-ADM/src/Pages/GerenciamentoIa.jsx
@@ -6,20 +6,20 @@ import { ContainerLista } from "../components/ContainerLista";
 const GerenciamentoIa = () => {
   return (
      <>
-        <div className='container mt-2 pt-2 mb-3 pb-3'>
+     <div className='container py-5 mt-2 pt-2 mb-3 pb-3'>
             <h3 className='text-capitalize fw-bold text-start m-0'>Monitoramento de uso da IA</h3>
             <p className='fs-6 fw-light'>Dashboard com métricas de utilização da IA do projeto</p>
     
-        <div className='d-flex flex-column overflow-hidden'>
-        <div className="border border-2 border-dark border-opacity-10 shadow d-flex flex-column rounded-4 px-3 py-3">
-<Search/>
-        <ContainerLista 
-        topico={"Listagem Detalhada de Requisições"}
-        desc={"Histórico completo de requisições processadas"}
-        lista={<ListaProjetos />}
-        />
-        </div>
-        </div>
+            <div className='d-flex flex-column gap-3 overflow-hidden'>
+                <div className="border border-2 border-dark border-opacity-10 shadow d-flex flex-column rounded-4 px-3 py-3">
+                    <Search/>
+                </div>
+                <ContainerLista 
+                    topico={"Listagem Detalhada de Requisições"}
+                    desc={"Histórico completo de requisições processadas"}
+                    lista={<ListaProjetos />}
+                />
+            </div>
         </div>
         </>
   )
