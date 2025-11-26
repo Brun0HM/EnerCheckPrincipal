152d287182b03daed59eb02046c5070444b07ef6
Luiz G. <luiz.mwlo76@gmail.com>
2025-11-26 14:17:59 -0300

    re-adicionando interceptors para os endpoints

--- FronEnd/services/api.js
+++ FronEnd/services/api.js
@@ -9,6 +9,50 @@ const api = axios.create({
   timeout: 10000,
 });
 
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("Token");
+  if(token) {
+    config.headers['Authorization'] = `Bearer ${token}`;
+  }
+
+  return config;
+}, (error) => {
+  return Promise.reject(error);
+})
+
+
+
+api.interceptors.response.use((response) => {
+  return response;
+}, async (error) => {
+  const originalRequest = error.config;
+
+  if (error.response?.status === 401 && !originalRequest._retry) {
+    originalRequest._retry = true;
+    try {
+      const refreshToken = localStorage.getItem('refreshToken');
+
+      const response = await api.post("/Usuario/refresh", {
+        refreshToken: refreshToken
+      });
+
+      const novoToken = response.data.accessToken;
+      localStorage.setItem('Token', novoToken);
+      originalRequest.headers['Authorization'] = `Bearer ${novoToken}`;
+      return api(originalRequest);
+    } catch (refreshError) {
+      console.error('O refresh token falhou: ', refreshError);
+      localStorage.clear();
+      window.location.href = '/'
+       
+      
+    }
+  }
+} 
+
+)
+
 // 1. Usuarios --------------------------------------------------------------------------------
 /**
  * Função para buscar os perfis.
